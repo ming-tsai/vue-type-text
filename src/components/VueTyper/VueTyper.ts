c2eb15c1a1b2cd63a9479fe6b97eb80508fb8f1c
Ming Tsai <ming.jia1213@gmail.com>
2020-06-17 19:57:51 -0400

    chore(prettier): formatter the code

--- src/components/VueTyper/VueTyper.ts
+++ src/components/VueTyper/VueTyper.ts
@@ -29,81 +29,76 @@ export default class VueTyper extends Vue {
     await this.writeInit();
   }
 
-  delay(ms: number) {
+  private delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
   private isDone = false;
-  private timer!: any;
   private arrayIndex = 0;
   private bindingText = "";
-  async writeInit() {
+  private async writeInit() {
     if (typeof this.text === "string") {
       await this.writeText(this.text);
       await this.delay(this.timeTakes);
       this.isDone = true;
     } else {
-      if(Array.isArray(this.text))
-      {
-        this.setIntervalImmediately(async () => {
-          await this.writeText(this.text[this.arrayIndex]);
-          await this.delay(this.timeTakes * 1.5);
-          await this.eraseText(this.text[this.arrayIndex]);
-          if(this.arrayIndex >= this.text.length - 1) {
-            this.arrayIndex = 0;
-          }else {
-            this.arrayIndex++;
-          }
-          return;
-        }, this.timeTakes * 1.4 * 2);
+      if (Array.isArray(this.text)) {
+        this.setIntervalImmediately(
+          async () => await this.managerArray(),
+          this.timeTakes * 1.4 * 2
+        );
       }
     }
   }
 
-  setIntervalImmediately(func: any, interval: number) {
+  private async managerArray() {
+    await this.writeText(this.text[this.arrayIndex]);
+    await this.delay(this.timeTakes * 1.5);
+    await this.eraseText(this.text[this.arrayIndex]);
+    if (this.arrayIndex >= this.text.length - 1) {
+      this.arrayIndex = 0;
+    } else {
+      this.arrayIndex++;
+    }
+    return;
+  }
+
+  private setIntervalImmediately(func: () => void, interval: number) {
     func();
     return setInterval(func, interval);
   }
 
-  async writeText(text: string) {
+  private async writeText(text: string) {
     let index = 0;
     this.bindingText = "";
     const period = this.timeTakes / text.length;
     const timer = setInterval(() => {
       if (index >= text.length - 1) {
         clearInterval(timer);
-      } 
-      
+      }
       index = this.appendText(text[index], index);
     }, period);
   }
 
-  appendText(char: string, index: number) {
+  private appendText(char: string, index: number) {
     this.bindingText += char;
     index++;
     return index;
   }
 
-  eraseInit() {
-    if (typeof this.text === "string") {
-      this.eraseText(this.text);
-    }
-  }
-
-  async eraseText(text: string) {
+  private async eraseText(text: string) {
     let index = text.length;
     this.bindingText = text;
     const period = this.timeTakes / text.length;
     const timer = setInterval(() => {
       if (index <= 0) {
         clearInterval(timer);
-      } 
-      
+      }
       index = this.removeText(index);
     }, period);
   }
 
-  removeText(index: number) {
+  private removeText(index: number) {
     this.bindingText = this.bindingText.substring(0, index);
     index--;
     return index;
