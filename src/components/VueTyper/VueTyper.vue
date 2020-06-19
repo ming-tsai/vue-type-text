d5504c4f58016b3abc9ff52994e84af50808f4f9
Ming Tsai <ming.jia1213@gmail.com>
2020-06-18 21:42:49 -0400

    feat: delete all project

--- src/components/VueTyper/VueTyper.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <component :is="tag">
-    <span class="s-typer">{{ bindingText }}</span>
-    <span class="caret" v-if="!isDone"></span>
-  </component>
-</template>
-
-<script lang="ts" src="./VueTyper.ts" />
-<style src="./VueTyper.css" scoped />
